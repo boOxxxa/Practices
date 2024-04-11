532c5684e7e25473b8a8f3bf0ee6e72f322e92b7
boOxxxa <hairudinov.bogdan@gmail.ru>
2024-04-11 20:04:53 +0400

    z4

--- Practice_2/Task23.py
+++ Practice_2/Task23.py
@@ -1,34 +1,4 @@
-# import numpy as np
-# from scipy.optimize import minimize_scalar
-#
-# # Определение функции f(t)
-# def f(t, v):
-#     return np.sin(v * np.cos(2 * np.pi * t))
-#
-# # Функция для вычисления спектрального веса для заданной частоты
-# def spectral_weight(freq, v):
-#     t = np.linspace(0, 1, 1000)  # временная ось
-#     signal = f(t, v)
-#     spectrum = np.abs(np.fft.fft(signal))
-#     return spectrum[int(freq)]
-#
-# # Функция для нахождения частоты с максимальным спектральным весом
-# def find_max_spectrum_freq(v):
-#     # Минимизация отрицательного спектрального веса, чтобы найти максимальный
-#     result = minimize_scalar(lambda freq: -spectral_weight(freq, v), bounds=(0, 10), method='bounded')
-#     return result.x
-#
-# # Ввод значения v с клавиатуры
-# v = float(input("Введите значение v: "))
-#
-# # Поиск частоты с максимальным спектральным весом
-# max_freq = find_max_spectrum_freq(v)
-# print("Неотрицательная частота с максимальным спектральным весом:", max_freq)
-# for i in range(1,100):
-#     if(find_max_spectrum_freq(i)<6):
-#         print("Максимальный спектральный вес для v =", i, ":", round(find_max_spectrum_freq(i), 2))
-#     #print("Максимальный спектральный вес для v =", i, ":", round(spectral_weight(i), 2))
-# print("Максимальный спектральный вес для v =", v, ":", round(max_freq, 2))
+
 import numpy as np
 
 # Определение функции f(t)
