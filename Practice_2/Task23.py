75e9466a0f84fe778456b1a3cd255009959b2a9b
boOxxxa <hairudinov.bogdan@gmail.ru>
2024-04-11 20:07:11 +0400

    z4

--- Practice_2/Task23.py
+++ Practice_2/Task23.py
@@ -1,21 +1,18 @@
-
 import numpy as np
 
-# Определение функции f(t)
 def f(t, v):
     return np.sin(v * np.cos(2 * np.pi * t))
 
-# Функция для вычисления спектрального веса для заданной частоты
+
 def spectral_weight(freq, v):
-    t = np.linspace(0, 10, 100)  # временная ось
+    t = np.linspace(0, 10, 100)
     signal = f(t, v)
     spectrum = np.abs(np.fft.fft(signal))
     freq_index = int(freq * len(t))
     return spectrum[freq_index]
 
-# Функция для нахождения частоты с максимальным спектральным весом
 def find_max_spectrum_freq(v):
-    t = np.linspace(0, 100,100)  # временная ось
+    t = np.linspace(0, 100,100)
     freqs = np.fft.fftfreq(len(t))
     signal = f(t, v)
     spectrum = np.abs(np.fft.fft(signal))
@@ -23,9 +20,9 @@ def find_max_spectrum_freq(v):
     max_freq = freqs[max_freq_index]
     return max_freq
 
-# Ввод значения v с клавиатуры
+
 v = float(input("Введите значение v: "))
 
-# Поиск частоты с максимальным спектральным весом
+
 max_freq = find_max_spectrum_freq(v)
 print("Неотрицательная частота с максимальным спектральным весом:", max_freq)
